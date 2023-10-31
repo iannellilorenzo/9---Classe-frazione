8c10fcb184d6e0ea4733285715e105b253d20f1c
Iannelli.20192 <Iannelli.20192@E2-9-PC05>
2023-10-31 13:56:00 +0100

    Aggiungere i file di progetto.

---
+++ 9 - Classe frazione/Frazione.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9___Classe_frazione
+{
+    public class Frazione
+    {
+        private int _numeratore;
+        private int _denminatore;
+
+        public Frazione()
+        {
+
+        }
+    }
+}
