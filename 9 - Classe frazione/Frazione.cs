7ddac7e36b0a5db1e6690e03fa3d4d1f0e1ae78a
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-10-31 15:09:08 +0100

    get/set, costuttori, clone

--- 9 - Classe frazione/Frazione.cs
+++ 9 - Classe frazione/Frazione.cs
@@ -9,11 +9,52 @@ namespace _9___Classe_frazione
     public class Frazione
     {
         private int _numeratore;
-        private int _denminatore;
+        private int _denominatore;
+
+        public int Numeratore
+        {
+            get { return _numeratore; }
+            set { _numeratore = value; }
+        }
+
+        public int Denominatore
+        {
+            get { return _denominatore; }
+            set
+            {
+                if (value != 0)
+                {
+                    _denominatore = value;
+                }
+                else
+                {
+                    _denominatore = 1;
+                }
+            }
+        }
 
         public Frazione()
         {
+            Numeratore = 1;
+            Denominatore = 1;
+        }
 
+        public Frazione(int numer, int denom)
+        {
+            Numeratore = numer;
+            Denominatore = denom;
+        }
+
+        public Frazione(Frazione frazioneVecchia)
+        {
+            Numeratore = frazioneVecchia.Numeratore;
+            Denominatore = frazioneVecchia.Denominatore;
+        }
+
+        public object Clona(Frazione frazioneVecchia)
+        {
+            Frazione fraz = (Frazione)frazioneVecchia.MemberwiseClone();
+            return fraz;
         }
     }
 }
